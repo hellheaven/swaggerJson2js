6f74c4c5717c6a1043852f754f57a971d2432eaa
Colin.Yu <7375072@qq.com>
2022-01-28 13:54:48 +0800

    更新依赖包

--- gen.js
+++ gen.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 const axios = require('axios');
 const path = require('path')
 const converter = require('swagger2openapi');
-const toJsonSchema = require('@openapi-contrib/openapi-schema-to-json-schema');
+const toJsonSchema = require('openapi-schema-to-json-schema');
 import {
     Resolver
-} from "@stoplight/json-ref-resolver";
+} from "json-ref-resolver";
 const template = require('./template').vueTemplate;
 
 const args = process.argv.slice(2)
-let swaggerFile = args[0] ? args[0] : 'api-docs.json'//可以是网址
+let swaggerFile = args[0] ? args[0] : 'http://kz.gdwstech.com/crm/v2/api-docs?group=web%E6%8E%A5%E5%8F%A3%E6%96%87%E6%A1%A3' //可以是网址
 const exportFile = args[1] ? args[1] : 'apis.js'
 const tarDir = 'dist/'
 
@@ -44,7 +44,7 @@ function analysis(data) {
         console.log(apiName)
         let apiMethod = Object.keys(data.paths[apiName])[0]
         let apiSummary = Object.values(data.paths[apiName])[0].summary
-        res += `//${apiSummary} \n export const ${apiName.match(/\w*$/)} = (data) => http.${apiMethod}("${apiName}",data);\n`
+        res += `//${apiSummary} \n export const ${apiName.match(/\w*$/)} = (data) => http.${apiMethod}({url:"${apiName}",params:data});\n`
     }
     saveFile(res)
 
@@ -70,7 +70,9 @@ async function coverHandler(err, res) {
         let method = Object.keys(forms[item])[0]
         if (method == 'post' && forms[item].post.requestBody) {
             let name = item.match(/\w*$/)[0]
-            saveVueTemplete(name, 'schemas/' + name, JSON.stringify(forms[item].post.requestBody.content['application/json'].schema), forms[item].post.summary)
+            if (forms[item].post.requestBody.content['application/json']){
+                saveVueTemplete(name, 'schemas/' + name, JSON.stringify(forms[item].post.requestBody.content['application/json'].schema), forms[item].post.summary)
+            }
         }
     }
     saveFile(JSON.stringify(resolved.result), 'jsonSchema.json')
